5fe0249ee66672f8d0a5132823f59b424b1147b4
mike <apolloproductionsdj@gmail.com>
2021-07-19 08:00:09 -0700

    Initial commit

---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Header() {
+    return (
+        <div>
+            {/* HeaderLeft */}
+
+            {/* HeaderCenter */}
+
+            {/* HeaderRight */}
+
+        </div>
+    )
+}
+
+export default Header;
+
+const HeaderContainer = styled.div`
+    color: "red";
+`;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA1onslFnfXdEcCoeX_eQIOPkwJQHHmEXA",
+    authDomain: "slack-clone-2-91b32.firebaseapp.com",
+    projectId: "slack-clone-2-91b32",
+    storageBucket: "slack-clone-2-91b32.appspot.com",
+    messagingSenderId: "44833977975",
+    appId: "1:44833977975:web:7b31b2ff1a007a5e64b872"
+};
